ef25cb3c8e4f464ea3765eb202b0af055d730ed6
Danilo Treffiletti <urban82@gmail.com>
2014-08-07 21:53:31 +0200

    Implementation

--- data/ShowPasswordField.js
+++ data/ShowPasswordField.js
@@ -31,3 +31,96 @@ self.port.on("pref-changed", function(prefs) {
             break;
     }
 });
+
+function ShowPasswordField(obj) {
+    var self = this;
+    self.obj = obj;
+    self.showed = false;
+    self.hiddenWithoutExit = false;
+    self.showTimer = null;
+    self.hideTimer = null;
+
+    self.foreground = self.obj.style.color;
+    self.background = self.obj.style.background;
+
+    self.onEnter = function() {
+        if (self.hiddenWithoutExit) return; // Avoid re-show after an hide for blur
+        // Cancel hide action
+        if (self.hideTimer != null) {
+            window.clearTimeout(self.hideTimer);
+            self.hideTimer = null;
+        }
+        // Set show action
+        if (self.showTimer == null) {
+            self.showTimer = window.setTimeout(
+                function() {
+                    self.showTimer = null;
+                    self.show();
+                },
+                showDelay * 1000
+            );
+        }
+    };
+    self.onExit = function() {
+        // Cancel show action
+        if (self.showTimer != null) {
+            window.clearTimeout(self.showTimer);
+            self.showTimer = null;
+        }
+        // Set hide action
+        if (self.hideTimer == null) {
+            self.hideTimer = window.setTimeout(
+                function() {
+                    self.hideTimer = null;
+                    self.hide();
+                    self.hiddenWithoutExit = false;
+                },
+                hideDelay * 1000
+            );
+        }
+    };
+
+    self.change = function(type, fg, bg) {
+        // Save selection
+        var selectionStart = self.obj.selectionStart;
+        var selectionEnd = self.obj.selectionEnd;
+
+        // Apply custom colors
+        if (customColors) {
+            self.obj.style.color = fg;
+            self.obj.style.background = bg;
+        }
+
+        // Change field type
+        self.obj.type = type;
+
+        // Restore selection
+        self.obj.selectionStart = selectionStart;
+        self.obj.selectionEnd = selectionEnd;
+    };
+    self.show = function() {
+        if (!self.showed) {
+            self.change("text", foreground, background);
+            self.showed = true;
+        }
+    };
+    self.hide = function() {
+        if (self.showed) {
+            self.change("password", self.foreground, self.background);
+            self.showed = false;
+            self.hiddenWithoutExit = true;
+        }
+    };
+
+    self.obj.addEventListener("mouseover", self.onEnter, false);
+    self.obj.addEventListener("mouseout", self.onExit, false);
+    self.obj.addEventListener("blur", self.hide, false);
+}
+
+var passwordFields = document.evaluate('//input[@type="password"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+var num_passwordFields = passwordFields.snapshotLength;
+for (var i = 0; i < num_passwordFields; ++i) {
+    console.log("Found - " + passwordFields.snapshotItem(i).name);
+    new ShowPasswordField(passwordFields.snapshotItem(i));
+}
+console.log("Setup done for " + num_passwordFields + " password fields");
